45e7aac6339bfb409f3adf28113d69ed287208e1
Steve Doggett <artigat1@users.noreply.github.com>
2018-11-16 13:05:59 +0000

    Display the itms added to the backlog
    Listens to updates to the todo items in the store

--- src/components/Backlog.vue
+++ src/components/Backlog.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="backlog-view">
-    <p>Backlog</p>
     <new-item></new-item>
+
+    <div class="card" v-for="item in items" :key="item.id">
+      <div class="card-block">
+        <h5 class="card-title"><span class="text-muted">#{{item.id}}</span>
+          {{item.text}}</h5>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 import NewItemForm from './NewItemForm';
 
 export default {
@@ -13,5 +21,8 @@ export default {
     components: {
         'new-item': NewItemForm,
     },
+    computed: mapState({
+        items: s => s.items.todo,
+    }),
 };
 </script>

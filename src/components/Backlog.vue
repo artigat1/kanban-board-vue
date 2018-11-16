475311057d42c9549cf6b9fff9f42d41fdfe2cc4
Steve Doggett <artigat1@users.noreply.github.com>
2018-11-16 12:41:44 +0000

    Added a form to collect new items
    Added the form to the backlog page and routing for that to be the default page.

---
+++ src/components/Backlog.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="backlog-view">
+    <p>Backlog</p>
+    <new-item></new-item>
+  </div>
+</template>
+
+<script>
+import NewItemForm from './NewItemForm';
+
+export default {
+    name: 'Backlog',
+    components: {
+        'new-item': NewItemForm,
+    },
+};
+</script>
